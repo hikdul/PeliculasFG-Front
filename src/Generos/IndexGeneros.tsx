5b52b8e244df3c9bb0de5b2e9e24cebee86fd62c
Hector Contreras <hikdul.lio@gmail.com>
2022-06-09 15:06:26 -0500

    mi nemesis la paginacion && imcompleta

--- src/Generos/IndexGeneros.tsx
+++ src/Generos/IndexGeneros.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import Button from "../utils/Button";
 import { GENEROS } from "../utils/endpoints";
 import ListadoGenerico from "../utils/ListadoGenerico";
+import Paginacion from "../utils/paginacion";
 import { generoDTO } from "./Generos.model";
 
 
@@ -11,21 +12,36 @@ import { generoDTO } from "./Generos.model";
 export default function IndexGeneros()
 {
     const [generos,setGeneros]= useState<generoDTO[]>()
+    const [totalPag, setTotalPag]=useState(0)
+    const [recordsPorPagina, SetRecordsPorPagina]=useState(3)
+    const [pagina, setPagina]=useState(1)
+
     //asi se usa axios    
     useEffect(()=>{
         console.log('Peticion a => ' + GENEROS)
-        axios.get(GENEROS)
-            .then((resp:AxiosResponse<generoDTO[]>)=>{
+        axios.get(GENEROS, {
+            params:{pagina, recordsPorPagina}
+            
+        })
+            .then((resp:AxiosResponse<generoDTO[]>) => {
+                const totalDeRegistro = 
+                    parseInt(resp.headers['cantidadtotalregistros'],10)
+                setTotalPag(Math.ceil(totalDeRegistro/recordsPorPagina))
                 console.info(resp.data)
                 setGeneros(resp.data)
             })
-    },[])
+    },[pagina,recordsPorPagina])
     //el objeto vacio es para que el axio no se conecte si no una sola ves
     return(
         <>
             <Link className="btn btn-dark" to="Generos/Create">Nuevo</Link>
             <h3> Generos</h3>
 
+            <Paginacion
+                cantidadTotalDePaginas={totalPag}
+                paginaActual={pagina}
+                onChange={nuevaPagina=>setPagina(nuevaPagina)}/>
+                
             <ListadoGenerico listado={generos}>
                 <div className="table-responsive">
                     <table className="table table-striped">
---
+++ src/utils/paginacion.tsx
@@ -0,0 +1,100 @@
+import { prependOnceListener } from "process"
+import { useEffect, useState } from "react"
+
+
+export default function Paginacion(props: paginacionProps)
+{
+
+    const [listLinks, setListLinks]= useState<modeloLink[]>([])
+
+    useEffect(()=>{
+        const paginaAnteriorHabilitada= props.paginaActual!==1
+        const PaginaAnterior = props.paginaActual - 1
+        
+        const links: modeloLink[]=[]
+
+        links.push({
+            texto:'Prev',
+            habilitado: paginaAnteriorHabilitada,
+            pagina : PaginaAnterior,
+            activo : false
+        })
+        
+        for (let i = 0; i <= props.cantidadTotalDePaginas.length; i++) {
+            if(i>= props.paginaActual - props.radio && i <= props.paginaActual + props.radio)
+                links.push({
+                    texto:  i.toString(),
+                    activo: props.paginaActual===i,
+                    habilitado: true,
+                    pagina: i
+                })
+        }
+
+        const paginaSiguienteHabilitada = props.paginaActual !== props.cantidadTotalDePaginas && props.cantidadTotalDePaginas>0
+        const paginaSiguiente = props.paginaActual + 1
+        links.push({
+            texto: 'Next',
+            pagina: paginaSiguiente,
+            habilitado: paginaAnteriorHabilitada,
+            activo: false
+        })
+        setListLinks(links)
+    },[props.paginaActual, props.cantidadTotalDePaginas,props.radio])
+// este array anterior le indica al uso effect que cada ves que cambie el valor de uno de estos elementos, 
+// se dispare de nuevo el use Effect
+
+    function getClass(link:modeloLink)
+    {
+        if(link.activo)
+            return 'active pointer'
+        if(!link.habilitado)
+            return 'disabled'
+        return 'pointer'
+    }
+
+    function seleccionaPagina(link:modeloLink)
+    {
+        if(link.pagina===props.paginaActual)
+            return
+        if(!link.habilitado)
+            return
+        props.onChange(link.pagina)
+    }
+
+    return(
+        <nav>
+            <ul className="pagination justify-content-center">
+                {listLinks.map(link => 
+                    <li 
+                        key={link.texto}
+                        onClick={()=>{seleccionaPagina(link)}}
+                        className={`page-item cursor ${getClass(link)}`}>
+                            <span className="page-link">{link.texto}</span>
+                    </li>
+                    )}
+            </ul>
+        </nav>
+    )
+
+
+}
+
+interface modeloLink
+{
+    pagina:number
+    habilitado: boolean
+    texto: string
+    activo: boolean
+}
+
+interface paginacionProps
+{
+    paginaActual: number
+    cantidadTotalDePaginas: number
+    radio: number
+    onChange(pagina:number): void
+}
+
+Paginacion.defaultProps={
+    radio: 3
+}
